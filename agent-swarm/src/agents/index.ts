a8d1f71a14a553073a53316f6827ce98a5351bc8
guyster323 <window98se@gmail.com>
2026-02-09 07:17:47 +0900

    Delete agent-swarm directory

--- agent-swarm/src/agents/index.ts
+++
@@ -1,8 +0,0 @@
-// Agent Exports
-
-export { BaseAgent } from './base.js';
-export { PlannerAgent } from './planner.js';
-export { CoderAgent } from './coder.js';
-export { ReviewerAgent } from './reviewer.js';
-export { TesterAgent } from './tester.js';
-export { DebuggerAgent } from './debugger.js';
--- agent-swarm/src/core/index.ts
+++
@@ -1,3 +0,0 @@
-// Core Exports
-
-export { SwarmOrchestrator, createSwarm } from './orchestrator.js';
